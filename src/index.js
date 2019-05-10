117f73c0ae6d1a05dcb9f9926b6c6776e3188478
Aleksandr Seymor <godseymor@gmail.com>
2019-05-10 16:12:19 +0500

    Добавляет проп-тайпсы компонентам.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Main} from "./components/main/main.jsx";
+import Main from "./components/main/main.jsx";
 
 const init = () => {
 
+  const movies = [
+    `Fantastic Beasts`,
+    `Bohemian Rhapsody`,
+    `Macbeth`,
+    `Aviator`
+  ];
+
   ReactDOM.render(
-      <Main />,
+      <Main
+        movies = {movies}
+      />,
       document.querySelector(`#root`)
   );
 };
